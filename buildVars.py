d0c9d3f7c192c3fa4518c9cb949291bc1e99bdee
ogomez92 <meow@meow.com>
2020-03-19 09:49:31 +0100

    changed description and summary

--- buildVars.py
+++ buildVars.py
@@ -14,22 +14,22 @@ addon_info = {
 	"addon_name" : "winWizard",
 	# Add-on summary, usually the user visible name of the addon.
 	# Translators: Summary for this add-on to be shown on installation and add-on information.
-	"addon_summary" : _("winWizard"),
+	"addon_summary" : _("winWizard: Show and hide windows, browse windows of the same process, kill processes"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description" : _("""Provides hiding and unhiding of windows + other functions"""),
+	"addon_description" : _("""This addon allows you to hide windows (windows shift and numbers). Also, allows exploration of top level windows (windows+tab), and kill a non responding process with windows f4."""),
 	# version
 	"addon_version" : "4.0.0",
 	# Author(s)
 	"addon_author" : u"Oriol Gomez <ogomez.s92@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url" : None,
+	"addon_url" : "https://www.oriolgomez.com",
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion" : "2019.3.0",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2019.3.0",
+	"addon_lastTestedNVDAVersion" : "2019.3.1",
 	# Add-on update channel (default is None, denoting stable releases, and for development releases, use "dev"; do not change unless you know what you are doing)
 	"addon_updateChannel" : None,
 }
