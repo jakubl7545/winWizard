36a2cffa336517cd937f836e80fcac9e1616cb83
Łukasz Golonka <lukasz.golonka@mailbox.org>
2022-04-06 16:43:13 +0200

    Update copyright header

--- addon/globalPlugins/winWizard/__init__.py
+++ addon/globalPlugins/winWizard/__init__.py
@@ -1,3 +1,11 @@
+# -*- coding: UTF-8 -*-
+
+# WinWizard add-on for NVDA
+# This file is covered by the GNU General Public License.
+# See the file COPYING.txt for more details.
+# Copyright (C) 2020-2022 Oriol Gomez <ogomez.s92@gmail.com>
+# Currently maintained by Łukasz Golonka <lukasz.golonka@mailbox.org>
+
 import globalVars
 import globalPluginHandler
 
